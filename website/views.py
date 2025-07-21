42826a658178f58b9359f6e354926487fec91c51
jayashri <jaysharipatil01@gmail.com>
2025-07-21 12:35:06 +0530

    done project

---
+++ website/views.py
@@ -0,0 +1,10 @@
+
+
+from django.shortcuts import render
+from blog.models import Article
+
+def index(request):
+    Articles = Article.objects.all()
+    return render(request, 'website/index.html',{'articles': Articles})
+
+
